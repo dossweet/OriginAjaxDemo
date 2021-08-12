799f259b697fbf0b75fa91fc6f780464750d18b1
何甜甜 <1731098360@qq.com>
2021-08-13 00:47:55 +0800

    适配移动端

--- reqTest/pswLogin.js
+++ reqTest/pswLogin.js
@@ -1,8 +1,23 @@
-let loginBtn = document.getElementsByClassName("loginBtn")[0];
+// 获取页面尺寸，用于判断是移动端还是PC端
+let pageWidth = document.body.clientWidth;
+let loginBtn = "";
+// 根据响应式布局分别获取数据
+if (pageWidth >= 768) {
+    loginBtn = document.getElementsByClassName("loginBtn")[0];
+}else{
+    loginBtn = document.getElementsByClassName("loginBtn")[1];
+}
 
 loginBtn.addEventListener('click', function () {
-    let emails = document.getElementsByClassName("email")[0].value;
-    let password = document.getElementsByClassName("psw")[0].value;
+    let emails = "";
+    let password = "";
+    if (pageWidth >= 768) {
+        emails = document.getElementsByClassName("email")[0].value;
+        password = document.getElementsByClassName("psw")[0].value;
+    }else{
+        emails = document.getElementsByClassName("email")[1].value;
+        password = document.getElementsByClassName("psw")[1].value;
+    }
     // 这里的ajax就是我封装后的XMLHttpRequest, 以参数的形式传递请求方式、请求参数和响应处理，URL是fastmock上做的一个接口
     ajax({
         type: 'POST',
